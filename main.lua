97c12f26fcfb48dda314946f3fbc8a1927205752
Benbebop <benbebop@hotmail.com>
2023-04-01 17:45:54 -0600

    list command

--- main.lua
+++ main.lua
@@ -6,8 +6,10 @@ if args[1]:lower() == "get" then
 	mode = 0
 elseif args[1]:lower() == "set" then
 	mode = 1
-elseif args[1]:lower() == "del" then
+elseif args[1]:lower() == "lst" then
 	mode = 2
+elseif args[1]:lower() == "del" then
+	mode = 3
 elseif args[1]:lower() == "help" then
 	process.stdout:write([[Usage: netrc [GET/SET/DEL] [OPTIONS]
 	
@@ -69,12 +71,59 @@ end
 
 local uv = require("uv")
 
-if mode == 0 then -- GET LOGIN DATA
-	local login = logins[options.machine]
-	if not login then
-		process.stdout:write("could not find matching machine")
-		return
+local login
+if options.machine then
+	logins[options.machine] = logins[options.machine] or {}
+	login = logins[options.machine]
+else
+	default = default or {}
+	login = default
+end
+
+if mode > 0 then
+	
+	if mode == 1 then
+		local charset = {n = 0} -- init charset
+		for char in ("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"):gmatch(".") do table.insert(charset, char) charset.n = charset.n + 1 end
+		
+		login.login = options.login or login.login
+		if options.password then
+			login.password = options.password:gsub("%%auto(%b[])", function(args)
+				local pass = {}
+				for i=1,32 do
+					math.randomseed(string.unpack("L", uv.random(4)))
+					pass[i] = charset[math.random(charset.n)]
+				end
+				return table.concat(pass)
+			end)
+		end
+	elseif mode == 3 then
+		logins[options.machine] = nil
 	end
+
+	local file = io.open(".netrc", "wb")
+
+	for machine,login in pairs(logins) do
+		local str = {"machine", machine}
+		for i,v in pairs(login) do
+			table.insert(str, i)
+			table.insert(str, v)
+		end
+		file:write(table.concat(str, " "), "\n")
+	end
+
+	if default then
+		local str = {"default"}
+		for i,v in pairs(default) do
+			table.insert(str, i)
+			table.insert(str, v)
+		end
+		file:write(table.concat(str, " "))
+	end
+	
+end
+
+if mode < 2 then
 	local str = {}
 	for i,v in pairs(login) do
 		table.insert(str, i)
@@ -83,43 +132,10 @@ if mode == 0 then -- GET LOGIN DATA
 		table.insert(str, "\n")
 	end
 	process.stdout:write(table.concat(str))
-	return
-elseif mode == 1 then
-	local charset = {n = 0} -- init charset
-	for char in ("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"):gmatch(".") do table.insert(charset, char) charset.n = charset.n + 1 end
-	logins[options.machine] = logins[options.machine] or {}
-	
-	logins[options.machine].login = options.login or logins[options.machine].login
-	if options.password then
-		logins[options.machine].password = options.password:gsub("%%auto(%b[])", function(args)
-			local pass = {}
-			for i=1,32 do
-				math.randomseed(string.unpack("L", uv.random(4)))
-				pass[i] = charset[math.random(charset.n)]
-			end
-			return table.concat(pass)
-		end)
-	end
-else
-	logins[options.machine] = nil
-end
-
-local file = io.open(".netrc", "wb")
-
-for machine,login in pairs(logins) do
-	local str = {"machine", machine}
-	for i,v in pairs(login) do
+elseif mode == 2 then
+	local str = {}
+	for i,v in pairs(logins) do
 		table.insert(str, i)
-		table.insert(str, v)
 	end
-	file:write(table.concat(str, " "), "\n")
-end
-
-if default then
-	local str = {"default"}
-	for i,v in pairs(default) do
-		table.insert(str, i)
-		table.insert(str, v)
-	end
-	file:write(table.concat(str, " "))
+	process.stdout:write(table.concat(str, "\n"))
 end
